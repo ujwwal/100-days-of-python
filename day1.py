cf3c5bc064c62191101f89239f43aadf33d9dba5
ujwwal <ujjwalguptamail@gmail.com>
2024-08-19 12:16:03 +0530

    test

---
+++ day1.py
@@ -0,0 +1,5 @@
+#band name gneenrator
+print("we will give you a band name today")
+city = input("what city u grew up in? \n")
+pet = input("what is the name of your pet? \n")
+final =  print(f"here is ur band name: \n", city +  " " + pet)
